a0fcfaddbe68dc5bb0ae6eff8ce0f182a199ebda
Beinsezii <beinsezii@gmail.com>
2020-10-21 14:57:15 -0700

    Save functionality
    Just gets a file name from FileChooserDialog and writes in unformatted
    text. Use regex to load.

--- color_thing.py
+++ color_thing.py
@@ -4,11 +4,11 @@ import gi
 gi.require_version("Gtk", "3.0")
 gi.require_version("Gdk", "3.0")
 from gi.repository import Gtk, Gdk
-from bszgw import Adjuster, Entry, Grid, GridChild as GC, App
+from bszgw import Adjuster, AutoBox, Button, CheckButton, Entry, Grid, GridChild as GC, App
 from discount_babl import Color
 
 
-LOREM_IPSUM_MINI = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
 
 class ColorAdjuster(Grid):
@@ -62,10 +62,10 @@ def gen_term_hues(lightness=50, chroma=50, hue=0) -> list:
     return [Color().set_LCH(lightness, chroma, 60*x+hue) for x in [0, 2, 1, 4, 5, 3]]
 
 
-def build_colors(fg: Color, bg: Color, lightness, chroma, hue, dim) -> list:
+def build_colors(fg: Color, bg: Color, lightness, chroma, hue, dim, oled) -> list:
     """Generates all 16 colors from Fg, Bg, and lch offsets"""
 
-    colors = [bg]
+    colors = [bg] if not oled else [Color(0, 0, 0)]
     colors += gen_term_hues(lightness, chroma, hue)
 
     bgdim = list(bg.as_LCH())
@@ -81,14 +81,32 @@ def build_colors(fg: Color, bg: Color, lightness, chroma, hue, dim) -> list:
     return colors
 
 
+def save_to_file(fg, bg, l, c, h, dim, oled):
+    chooser = Gtk.FileChooserDialog()
+    chooser.props.action = Gtk.FileChooserAction.SAVE
+    chooser.props.do_overwrite_confirmation = True
+    chooser.add_buttons(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_SAVE, Gtk.ResponseType.ACCEPT)
+
+    response = chooser.run()
+
+    if response == Gtk.ResponseType.ACCEPT:
+        data = list(fg.as_SRGB()) + list(bg.as_SRGB()) + [l, c, h, dim, oled]
+        with open(chooser.get_filename(), mode='w') as saveto:
+            saveto.write("FG_R:{:.1f}\nFG_G:{:.1f}\nFG_B:{:.1f}\n"
+                         "BG_R:{:.1f}\nBG_G:{:.1f}\nBG_B:{:.1f}\n"
+                         "L:{:.1f}\nC:{:.1f}\nH:{:.1f}\nDIM:{:.1f}\nOLED:{}".format(*data))
+
+    chooser.destroy()
+
+
 def main():
     # Main text boxes
-    main_box = Entry(label="Foreground/Background", value=LOREM_IPSUM_MINI, min_height=75)
+    main_box = Entry(label="Foreground/Background", value=LOREM_IPSUM, min_height=150)
     main_box.entry.props.wrap_mode = Gtk.WrapMode.WORD
     main_box.entry.props.editable = False
     main_box.entry.provider = Gtk.CssProvider()
 
-    dim_box = Entry(label="Foreground2/Background2", value=LOREM_IPSUM_MINI, min_height=75)
+    dim_box = Entry(label="Foreground2/Background2", value=LOREM_IPSUM, min_height=150)
     dim_box.entry.props.wrap_mode = Gtk.WrapMode.WORD
     dim_box.entry.props.editable = False
     dim_box.entry.provider = Gtk.CssProvider()
@@ -138,11 +156,20 @@ def main():
         override_color(dim_box.entry, fg=colors[15], bg=colors[8])
 
     def on_adj_change(widget):
-        set_all_colors(gen_colors())
+        set_all_colors(build_colors(*get_vals()))
+
+    def on_save(widget):
+        save_to_file(*get_vals())
+
+    def on_load(widget):
+        pass
+
+    def on_export(widget):
+        pass
 
     # Pickers
-    fg_adjuster = ColorAdjuster(Color(1.0, 1.0, 1.0))
-    bg_adjuster = ColorAdjuster(Color(0.0, 0.0, 0.0))
+    fg_adjuster = ColorAdjuster(Color(1, 1, 1))
+    bg_adjuster = ColorAdjuster(Color(0, 0, 0))
 
     # Adjusters
     l_adj = Adjuster.new("Colors Lightness", 50, -100, 100, 5, 10, 1)
@@ -153,31 +180,42 @@ def main():
     for w in fg_adjuster.adjusters+bg_adjuster.adjusters + [l_adj, c_adj, h_adj, d_adj]:
         w.adjustment.connect("value-changed", on_adj_change)
 
-    # convert UI vals to Color list
-    def gen_colors():
-        return build_colors(
+    save_button = Button("Save", on_save, tooltip="Save current vals to file")
+    load_button = Button("Load", on_load, tooltip="Load vals from file")
+    oled_toggle = CheckButton("OLED Mode", False, tooltip="Make the BG pure black")
+    export_button = Button("Export", on_export, tooltip="Export palette")
+    action_bar = AutoBox([save_button, load_button, oled_toggle, export_button], 5, 5, 0)
+    # action_bar = Grid()
+    # action_bar.attach_all(save_button, load_button, oled_toggle, export_button, direction=Gtk.DirectionType.RIGHT)
+
+    oled_toggle.connect('toggled', on_adj_change)
+
+    def get_vals():
+        return [
                 fg_adjuster.color,
                 bg_adjuster.color,
                 l_adj.value,
                 c_adj.value,
                 h_adj.value,
                 d_adj.value,
-                )
+                oled_toggle.value,
+                ]
 
     grid = Grid()
     # attach displays
     grid.attach_all(*term_color_labels, direction=Gtk.DirectionType.RIGHT)
     grid.attach_all(*term_colors_bright, row=1, direction=Gtk.DirectionType.RIGHT)
     grid.attach_all(*term_colors_dim, row=2, direction=Gtk.DirectionType.RIGHT)
-    grid.attach_all(GC(main_box, width=8), GC(dim_box, width=8))
+    grid.attach_all(GC(main_box, height=2, width=8), GC(dim_box, height=2, width=8))
 
     # attach janky-ass custom fg/bg adjusters
-    grid.attach_all(GC(Gtk.Label("Foreground/Background Adjusters"), width=2), direction=Gtk.DirectionType.RIGHT)
+    grid.attach_all(GC(Gtk.Label(label="Foreground/Background Adjusters"), width=2), direction=Gtk.DirectionType.RIGHT)
     grid.attach_all(GC(fg_adjuster, width=2), GC(bg_adjuster, width=2), column=8)
 
     # attach other adjusters
     grid.attach_all(l_adj, c_adj, row=3, direction=Gtk.DirectionType.RIGHT)
     grid.attach_all(h_adj, d_adj, row=4, direction=Gtk.DirectionType.RIGHT)
+    grid.attach_all(GC(action_bar, width=10))
     grid.props.margin=10
 
     app = App("Color Thing", grid)
