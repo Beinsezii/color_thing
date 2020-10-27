fed2d3aa0d0c963c7f12638e02eff0d028b7cbce
Beinsezii <beinsezii@gmail.com>
2020-10-26 19:01:52 -0700

    GIMP and X11 exporters complete

--- exporters/gimp.py
+++ exporters/gimp.py
@@ -1,5 +1,14 @@
-NAME="GIMP Palette"
-FORMAT=".gpl"
+NAME = "GIMP Palette"
+FORMAT = ".gpl"
 
-def EXPORT(*args):
-    return bytes(f"exporting GIMP palette... {args}", encoding="UTF-8")
+
+def EXPORT(colors: list, name: str, accent: int) -> bytes:
+    data = f"""\
+GIMP Palette
+Name: {name}
+Columns: 8
+#"""
+    for num, c in enumerate(colors):
+        r, g, b = c.as_IRGB()
+        data += "\n{:>3} {:>3} {:>3} color{}".format(r, g, b, num)
+    return bytes(data, encoding="UTF-8")
--- exporters/xresources.py
+++ exporters/xresources.py
@@ -1,6 +1,24 @@
-NAME="X11 Resources"
-FORMAT=".Xresources"
+NAME = "X11 Resources"
+FORMAT = ".Xresources"
 
-def EXPORT(*args):
-    return bytes(f"exporting .Xresources ... {args}", encoding="UTF-8")
 
+def EXPORT(colors: list, name: str, accent: int) -> bytes:
+    colors = [c.as_HEX() for c in colors]
+    data = f"""\
+*background:     {colors[0]}
+*foreground:     {colors[7]}
+*highlightColor: {colors[15]}
+*cursorColor:    {colors[accent]}
+"""
+    for num, c in enumerate(colors):
+        data += f"\n*color{num}:" + ' ' * (9 if num < 10 else 8) + f'{c}'
+
+    # Rofi. May as well set it regardless, doesn't hurt anything.
+    # rofi.colors-normal: bg, fg, bgalt, bgsel, fgsel
+    # rofi.colors-window: background, border, separator
+    data += f"""\n
+rofi.color-normal:  {colors[0]} {colors[7]} {colors[8]} {colors[accent]}
+rofi.colors-window: {colors[0]} {colors[7]} colors{7}
+"""
+
+    return bytes(data, encoding="UTF-8")
