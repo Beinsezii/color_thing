cf79c391192fdd4ab18582d2af048804b7ed9939
Beinsezii <beinsezii@gmail.com>
2020-11-01 14:01:40 -0800

    Renamed i3-x vars to match i3

--- exporters/i3-x.py
+++ exporters/i3-x.py
@@ -5,27 +5,27 @@ FORMAT = "config"
 def EXPORT(colors: list, name: str, accent: int) -> bytes:
     colors = [c.as_HEX() for c in colors]
     data = f"""\
-set_from_resource $cursor i3wm.cursorColor {colors[accent]}
+set_from_resource $accent i3wm.cursorColor {colors[accent]}
 set_from_resource $fg i3wm.foreground {colors[7]}
 set_from_resource $bg i3wm.background {colors[0]}
-set_from_resource $hl i3wm.highlightColor {colors[8]}
+set_from_resource $bgalt i3wm.highlightColor {colors[8]}
 
 # class                 border  background text indicator child_border
-client.focused          $cursor $cursor    $bg  $fg       $cursor
-client.focused_inactive $hl     $hl        $fg  $fg       $hl
-client.unfocused        $bg     $bg        $fg  $hl       $bg
-client.urgent           $fg     $fg        $bg  $cursor   $fg
-client.placeholder      $hl     $hl        $fg  $bg       $hl
+client.focused          $accent $accent    $bg  $fg       $accent
+client.focused_inactive $bgalt  $bgalt     $fg  $fg       $bgalt
+client.unfocused        $bg     $bg        $fg  $bgalt    $bg
+client.urgent           $fg     $fg        $bg  $accent   $fg
+client.placeholder      $bgalt  $bgalt     $fg  $bg       $bgalt
 client.background               $bg
 
 bar {{
     colors{{
         background $bg
         statusline $fg
-        separator $cursor
+        separator $accent
         inactive_workspace $bg $bg $fg
-        focused_workspace $cursor $cursor $bg
-        urgent_workspace $cursor $bg $fg
+        focused_workspace $accent $accent $bg
+        urgent_workspace $accent $bg $fg
         }}
 }}
 """
