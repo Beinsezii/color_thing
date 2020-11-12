ec86cbebdaf8926c4e745df58ded15ca832a3eca
Beinsezii <beinsezii@gmail.com>
2020-11-11 20:17:09 -0800

    Significant vim.py overhaul.
    Now has an extra ui that pops up upon creation to allow manually picking
    the highlight groups. Now you can have errors be red without yellow
    constants!

--- exporters/vim.py
+++ exporters/vim.py
@@ -1,44 +1,145 @@
 NAME = "Vim (Truecolor)"
 FORMAT = "{name}.vim"
 
-def col_reker_1to6(cols: list, ac: int) -> list:
-    # {{{
-    # trust me on this
-    new_colors = [cols[ac - 1]]
-    mod = 1 if ac % 2 else -1
-    new_colors.append(cols[ac + mod - 1])
 
-    cur = ac
-    for x in range(2):
-        cur += 2
-        if cur > 6:
-            cur -= 6
-        new_colors.append(cols[cur - 1])
-        new_colors.append(cols[cur + mod - 1])
-    return new_colors
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+import bszgw
+from color_thing import override_color, Display  # it *just works*, okay?
+
+# Tooltips for color chooser #
+# {{{
+TT_C = """\
+*Constant    any constant
+ String        a string constant: "this is a string"
+ Character    a character constant: 'c', '\n'
+ Number        a number constant: 234, 0xff
+ Boolean    a boolean constant: TRUE, false
+ Float        a floating point constant: 2.3e10"""
+
+TT_I = """\
+*Identifier	any variable name
+ Function	function name (also: methods for classes)"""
+
+TT_S = """\
+*Statement	any statement
+ Conditional	if, then, else, endif, switch, etc.
+ Repeat		for, do, while, etc.
+ Label		case, default, etc.
+ Operator	"sizeof", "+", "*", etc.
+ Keyword	any other keyword
+ Exception	try, catch, throw"""
+
+TT_P = """\
+*PreProc	generic Preprocessor
+ Include	preprocessor #include
+ Define		preprocessor #define
+ Macro		same as Define
+ PreCondit	preprocessor #if, #else, #endif, etc."""
+
+TT_T = """\
+*Type		int, long, char, etc.
+ StorageClass	static, register, volatile, etc.
+ Structure	struct, union, enum, etc.
+ Typedef	A typedef"""
+
+TT_SP = """\
+*Special	any special symbol
+ SpecialChar	special character in a constant
+ Tag		you can use CTRL-] on this
+ Delimiter	character that needs attention
+ SpecialComment	special things inside a comment
+ Debug		debugging statements"""
+
+TT_U = """\
+*Underlined	text that stands out, HTML links"""
+
+TT_E = """\
+*Error		any erroneous construct"""
+
+TT_TD = """\
+*Todo		anything that needs extra attention; mostly the
+                keywords TODO FIXME and XXX"""
+# }}}
+
+
+class colorPicker(bszgw.Grid):
+    # {{{
+    def __init__(self, colors: list, label, value, tooltip=""):
+        super(colorPicker, self).__init__(column_spacing=2, row_spacing=2)
+        self.colors = colors
+        self.buttons = []
+        for n, c in enumerate(self.colors):
+            button = bszgw.Button(f"{n}", self.on_button, n)
+            button.provider = Gtk.CssProvider()
+            override_color(button, fg=c, bg=colors[0])
+            self.buttons.append(button)
+
+        self.display = Display(label, value, tooltip)
+        override_color(self.display, fg=self.colors[value], bg=self.colors[0])
+        self.attach_all(bszgw.GridChild(self.display, width=8))
+        self.attach_all(*self.buttons[:8], row=1, direction=Gtk.DirectionType.RIGHT)
+        self.attach_all(*self.buttons[8:], row=2, direction=Gtk.DirectionType.RIGHT)
+
+    def on_button(self, widget, val):
+        val = val[0]
+        self.display.value = val
+        override_color(self.display, fg=self.colors[val], bg=self.colors[0])
+
+    @property
+    def value(self):
+        return self.display.value
     # }}}
 
 
-def color_rekerjigger(cols: list, ac: int) -> list:
+def choose_colors(cols: list, ac: int) -> dict:
     # {{{
-    """Takes colors list and re-aranges them around the accent color"""
-    # So basically the goal is, have the colors be in order of priority.
-    # So the most used color would be the accent obviously,
-    # then the second most used is the accent's compliment,
-    # switch to new pair same thing.
+    result = {}
+    constant = colorPicker(cols, "Constant", 3, TT_C)
+    identifier = colorPicker(cols, "Identifier", 6, TT_I)
+    statement = colorPicker(cols, "Statement", 4, TT_S)
+    preproc = colorPicker(cols, "PreProc", 2, TT_P)
+    type = colorPicker(cols, "Type", 5, TT_T)
+    special = colorPicker(cols, "Special", 14, TT_SP)
+    underlined = colorPicker(cols, "Underlined", 11, TT_U)
+    error = colorPicker(cols, "Error", 1, TT_E)
+    todo = colorPicker(cols, "Todo", 9, TT_TD)
 
-    if ac < 8:
-        return cols[0:1] + col_reker_1to6(cols[1:7], ac) + cols[7:9] + col_reker_1to6(cols[9:15], ac) + cols[15:]
-    else:  # use dim colors first. im just gonna copy paste it since im lazy
-        return cols[0:1] + col_reker_1to6(cols[9:15], ac) + cols[7:9] + col_reker_1to6(cols[1:7], ac) + cols[15:]
+    grid = bszgw.Grid()
+    grid.attach_all(bszgw.GridChild(Gtk.Label(
+        "Vim (Truecolor) export color picker. See tooltips."), width=3))
+    grid.attach_all(constant, preproc, underlined)
+    grid.attach_all(identifier, type, error, column=1)
+    grid.attach_all(statement, special, todo, column=2)
+
+    dialog = Gtk.Dialog()
+    dialog.get_content_area().add(grid)
+    dialog.get_content_area().show_all()
+    dialog.add_buttons(Gtk.STOCK_OK, Gtk.ResponseType.ACCEPT)
+    dialog.props.modal = True
+
+    dialog.run()
+
+    result["constant"] = constant.value
+    result["identifier"] = identifier.value
+    result["statement"] = statement.value
+    result["preproc"] = preproc.value
+    result["type"] = type.value
+    result["special"] = special.value
+    result["underlined"] = underlined.value
+    result["error"] = error.value
+    result["todo"] = todo.value
+
+    dialog.destroy()
+    return result
     # }}}
 
 
 def EXPORT(colors: list, name: str, accent: int) -> bytes:
-    if accent in [0, 7, 8, 15]:
-        accent = 3
+    # {{{
+    groups = choose_colors(colors, accent)
     colors = [c.as_HEX() for c in colors]
-    colors = color_rekerjigger(colors, accent)
     data = f"""\
 hi clear
 if exists("syntax_on")
@@ -52,11 +153,12 @@ let s:fg = '{colors[7]}'
 let s:bg = '{colors[0]}'
 let s:fga = '{colors[15]}'
 let s:bga = '{colors[8]}'
+let s:ac = '{colors[accent]}'
 
 " Numbered colors are in order of priority, with lower being higher. Used for syntax.
 """
-    for num, var in enumerate(list(range(1, 7)) + list(range(9, 15))):
-        data += f"let s:c{num} = '{colors[var]}'\n"
+    for k in groups:
+        data += f"let s:{k} = '{colors[groups[k]]}'\n"
 
     data += """\
 
@@ -66,14 +168,14 @@ exe 'hi Normal guifg='.s:fg.' guibg='.s:bg
 exe 'hi NormalFloat guifg='.s:fg.' guibg='.s:bga
 exe 'hi NormalNC guifg='.s:fga
 
-exe 'hi Cursor guifg='.s:bg.' guibg='.s:c0
+exe 'hi Cursor guifg='.s:bg.' guibg='.s:ac
 hi! link LineNr NormalNC
-exe 'hi CursorLineNr guifg='.s:c0
+exe 'hi CursorLineNr guifg='.s:ac
 hi! link NonText LineNr
 
-exe 'hi Visual guifg='.s:bg.' guibg='.s:fg
-exe 'hi Search guifg='.s:bg.' guibg='.s:c1
-exe 'hi IncSearch guifg='.s:bg.' guibg='.s:c2.' gui=NONE'
+exe 'hi Visual guifg='.s:bg.' guibg='.s:fga
+exe 'hi Search guifg='.s:bg.' guibg='.s:identifier
+exe 'hi IncSearch guifg='.s:bg.' guibg='.s:type.' gui=NONE'
 
 exe 'hi Folded guifg='.s:bga.' guibg='.s:fga
 
@@ -82,24 +184,19 @@ exe 'hi SignColumn guibg='.s:bga
 exe 'hi Comment guifg='.s:fga
 
 " ## Syntax ##
-exe 'hi Constant guifg='.s:c0
-exe 'hi Underlined guifg='.s:c6.' guisp='.s:c0
-
-exe 'hi Statement guifg='.s:c1
-
-exe 'hi Type guifg='.s:c2
-
-exe 'hi Identifier guifg='.s:c3
-
-exe 'hi Error guifg='.s:c4.' guibg=NONE gui=bold'
-exe 'hi Special guifg='.s:c10.' gui=bold'
-
-exe 'hi PreProc guifg='.s:c5
-exe 'hi Todo guifg='.s:c11.' guibg=NONE gui=bold,undercurl guisp='.s:c5
+exe 'hi Constant guifg='.s:constant
+exe 'hi Identifier guifg='.s:identifier
+exe 'hi Statement guifg='.s:statement
+exe 'hi PreProc guifg='.s:preproc
+exe 'hi Type guifg='.s:type
+exe 'hi Special guifg='.s:special.' gui=bold'
+exe 'hi Underlined guifg='.s:underlined.' guisp='.s:underlined
+exe 'hi Error guifg='.s:error.' guibg=NONE gui=bold'
+exe 'hi Todo guifg='.s:todo.' guibg=NONE gui=bold'
 
 " ## Misc Built-in ##
 " ## Messages ##
-exe 'hi Question guifg='.s:c1.' guibg=NONE'
+exe 'hi Question guifg='.s:statement' guibg=NONE'
 hi! link ErrorMsg Error
 hi! link WarningMsg Special
 
@@ -114,3 +211,4 @@ exe 'hi PmenuThumb guibg='.s:fga
 """
 
     return bytes(data, encoding='UTF-8')
+    # }}}
